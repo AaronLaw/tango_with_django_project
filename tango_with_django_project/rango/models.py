939c00f29573da38830ca84c944c90221f7de1b4
aaronlaw <aaronlaw@gmail.com>
2017-02-13 21:53:32 +0800

    Define model Category and Page

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -3,3 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=128)
+
+	def __str__(self):
+		return self.name
+
+class page(models.Model):
+	Category = models.ForeignKey(Category)
+	title = models.CharField(max_length=128)
+	url = models.URLField()
+	views = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.title
