9166cb66240a8a64f3104ddc15596b1d3da3f480
aaronlaw <aaronlaw@gmail.com>
2017-02-14 16:19:30 +0800

    Fix the plural form of Category

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -6,8 +6,12 @@ from django.db import models
 class Category(models.Model):
 	name = models.CharField(max_length=128)
 
-	def __str__(self):
-		return self.name
+
+	class Meta:
+		verbose_name_plural = 'Categories'
+		
+		def __str__(self):
+			return self.name
 
 class Page(models.Model):
 	Category = models.ForeignKey(Category)
