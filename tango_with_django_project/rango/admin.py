348614e9b2bcb75da3a0a94beb1340c7b7620e27
aaronlaw <aaronlaw@gmail.com>
2017-02-14 16:52:43 +0800

    Custom the admin interface of Page

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('title','Category','url','views')
+
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
