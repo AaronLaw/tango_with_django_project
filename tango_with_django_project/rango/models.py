a4c8671786f97c94e15343dd081f2b40143c8afd
aaronlaw <aaronlaw@gmail.com>
2017-02-15 11:58:19 +0800

    Add a slug field, nad override save() in Category model

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -7,12 +7,19 @@ class Category(models.Model):
 	name = models.CharField(max_length=128)
 	views = models.IntegerField(default=0)
 	likes = models.IntegerField(default=0)
+	slug = models.SlugField()
 
 	def __str__(self):
 		return self.name
 
 	class Meta:
 		verbose_name_plural = 'Categories'
+
+	# Override the save() of Category moder, for slugify the name
+	# In order to reverse url in view
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
 		
 
 class Page(models.Model):
