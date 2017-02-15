9ed77e36d6922aa9916649edfd2c92fe7bb7f113
aaronlaw <aaronlaw@gmail.com>
2017-02-15 19:12:22 +0800

    Create a form - add Category

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -3,6 +3,7 @@ from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^category/add$', views.add_category, name='add_category'), # Notice: this rule should be placed before P<category_name_slug>
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', 
     	views.show_category, name='show_category'),
     url(r'^about/$', views.about, name ='about'),
