fbc3566786d56a9fa0f1383069ac91b6c7377d15
aaronlaw <aaronlaw@gmail.com>
2017-02-23 19:23:28 +0800

    Correct the import of User from .auth.models

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
