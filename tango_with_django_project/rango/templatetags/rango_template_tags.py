10cece1e4649ac3a8f5a000e7cf1808e6e23f339
aaronlaw <aaronlaw@gmail.com>
2017-03-01 21:51:44 +0800

    Parameterised template tags. we use parameterisation to highlight which category we are looking at when visiting.

--- tango_with_django_project/rango/templatetags/rango_template_tags.py
+++ tango_with_django_project/rango/templatetags/rango_template_tags.py
@@ -4,5 +4,6 @@ from rango.models import Category
 register = template.Library()
 
 @register.inclusion_tag('rango/cats.html')
-def get_category_list():
-	return {'cats': Category.objects.all()}
+def get_category_list(cat=None):
+	return {'cats': Category.objects.all(), 
+		'act_cat': cat}	 # We use parameterisation to highlight which category we are looking at when visiting its page. Note the inclusion of cat parameter to get_category_list() , which is optional - and if yu don't pass in a category, None is used as the subsequent value.
