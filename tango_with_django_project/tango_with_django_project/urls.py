d6703c6bdb61689a658059395b0c7cf91bba6be7
aaronlaw <aaronlaw@gmail.com>
2017-03-02 23:44:10 +0800

    Modifying the Registration Flow.

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -17,10 +17,19 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import  include
 from rango import views
+from registration.backends.simple.views import RegistrationView
+
+# Create a new class that redirects the user to the index page,
+# if successful at logging
+class MyRegistrationView(RegistrationView):
+	def get_success_url(self, user):
+		return '/rango/' # if user register successfully, we take them to the main index page
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')), # maps any URLs starting with rango/ to be handled by the rango application
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name="registration_register"),
 ]
+
